0765ca74c6b5f67c289c592dfa80c68e6c408343
tarmes <traarmes@gmail.com>
2020-10-09 12:18:37 -0400

    smurfs displaying

--- smurfs/src/components/SmurfDisplay.js
+++ smurfs/src/components/SmurfDisplay.js
@@ -3,20 +3,30 @@ import { fetchSmurfs } from '../store/actions';
 import { connect } from 'react-redux';
 
 function SmurfDisplay(props) {
-    const { fetchSmurfs } = props;
+    const { fetchSmurfs, smurfs } = props;
 
     useEffect(() => {
         fetchSmurfs()
     }, [fetchSmurfs]);
 
     return (
-        null
+        <div>
+            <h1>hello</h1>
+            {smurfs.map((smurf) => (
+                <div>
+                    <h1>{smurf.name}</h1>
+                    <p>{smurf.age}</p>
+                    <p>{smurf.height}</p>
+                </div>
+            ))}
+            
+        </div>
     )
 }
 
 const mapStatetoProps = (state) => {
     return {
-        state
+        smurfs: state.smurfs
     }
 }
 
