46b1135370cee41cc4aea13568b6ed607e01169a
tarmes <traarmes@gmail.com>
2020-10-09 11:33:40 -0400

    store, reducer created and provided to app

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { reducer } from './store/reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk, logger)
+));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById("root")
+);
